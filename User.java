9c962fbe5d24f33b4406c6d8996fe4c621a68bc6
phai-zk <chisanupong.m34.1@gmail.com>
2026-03-24 15:27:32 +0700

    init

--- User.java
+++ User.java
@@ -4,13 +4,37 @@ public abstract class User {
     protected String password;
     protected String phone;
 
-    public boolean login() {
-        System.out.println("User logged in.");
+    public boolean login(String username, String password) {
+        if (username.length() <= 5) {
+            System.out.println("Fail to login");
+            return false;
+        }
+        userId = username.substring(0, 5);
+        this.username = username;
+        this.password = password;
+        phone = "000-000-0000";
+        System.out.println("Login Success! Welcome " + this.username);
+        return true;
+    }
+
+    public boolean register(String username, String phone, String password, String confirmPass) {
+        if (username.length() <= 5 && !password.equals(confirmPass) && phone.matches("^\\+?[1-9]\\d{1,10}$")) {
+            System.out.println("Fail to register");
+            return false;
+        }
+        userId = username.substring(0, 5);
+        this.username = username;   
+        this.password = password;
+        this.phone = phone;
+        System.out.println("Register Success! Welcome " + this.username);
         return true;
     }
 
     public void logout() {
-        System.out.println("User logged out.");
+        userId = "";
+        username = "";
+        password = "";
+        phone = "";
     }
 
     // --- Getters & Setters ---
